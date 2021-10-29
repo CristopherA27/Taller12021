934d8e2417e47a9fcb0fbc113e0e650a22e86e29
CristopherA27 <76230505+CristopherA27@users.noreply.github.com>
2021-10-29 16:03:57 -0300

    ingresosObjetos

--- Taller1_2d02021/src/logica/SystemI.java
+++ Taller1_2d02021/src/logica/SystemI.java
@@ -1,5 +1,10 @@
 package logica;
+import dominio.*;
+import logica.*;
 
-public class SystemI {
-
+public interface SystemI {
+	//lectura txt
+	public boolean ingresarCuenta(String nombreCuenta,String contraseña,String nick,int nivelCuenta,int rpCuenta,String region);
+	public boolean ingresarPersonaje(String nombrePersonaje,String rol,int recaudacion);
+	
 }
--- Taller1_2d02021/src/logica/SystemImpl.java
+++ Taller1_2d02021/src/logica/SystemImpl.java
@@ -1,5 +1,56 @@
 package logica;
 
-public class SystemImpl {
+import dominio.Cuenta;
+import dominio.Personaje;
 
+public class SystemImpl implements SystemI{
+	
+	private ListaCuentas lCuentas;
+	private ListaPersonajes lPersonajes;
+	private ListaSkins lSkins;
+	
+	public SystemImpl() {
+		lCuentas = new ListaCuentas(100);
+		lPersonajes = new ListaPersonajes(100);
+		lSkins = new ListaSkins(100);
+	}
+
+	public boolean ingresarCuenta(String nombreCuenta, String contraseña, String nick, int nivelCuenta, int rpCuenta,String region) {
+		Cuenta cuenta = lCuentas.buscar(nombreCuenta);
+		if(cuenta == null) {
+			cuenta = new Cuenta(nombreCuenta, contraseña, nick, nivelCuenta, rpCuenta, region);
+			boolean ingresado = lCuentas.ingresar(cuenta);
+			if(ingresado) {
+				return true;
+			}else {
+				return false;
+			}
+		}else {
+			throw new NullPointerException("La Cuenta con el nombre "+nombreCuenta+" ya existe");
+		}	
+	}
+	
+	public boolean ingresarPersonaje(String nombrePersonaje,String rol,int recaudacion) {
+		Personaje personaje = lPersonajes.buscar(nombrePersonaje);
+		if(personaje == null) {
+			personaje = new Personaje(nombrePersonaje, rol, recaudacion);
+			boolean ingresado = lPersonajes.ingresar(personaje);
+			if(ingresado) {
+				return true;
+			}else {
+				return false;
+			}
+		}else {
+			throw new NullPointerException("El persona con el nombre "+nombrePersonaje+" ya existe");
+		}
+		
+		
+	}
+	
+	
+	
+	
 }
+	
+
+
