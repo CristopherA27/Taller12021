a61118fcd3ca0d8ec06f9e910878453350c32173
CristopherA27 <76230505+CristopherA27@users.noreply.github.com>
2021-11-05 17:39:12 -0300

    arreglar

--- Taller1_2d02021/src/logica/App.java
+++ Taller1_2d02021/src/logica/App.java
@@ -17,10 +17,31 @@ public class App {
 			String nick = partes[2];
 			int nivelCuenta = Integer.parseInt(partes[3]);
 			int rpCuenta = Integer.parseInt(partes[4]);
-			String region = partes[5];
+			int cantPersonajes = Integer.parseInt(partes[5]);
+			int avance = 6;
+			int cont=0;
+			int cont2 = 0;
+			for(int i=0;i<cantPersonajes;i++) {
+				String nombrePersonaje = partes[avance+cont];
+				int cantSkins = Integer.parseInt(partes[7+cont]);
+				for(int j=0;j<cantSkins;j++) {
+					if(cantSkins<=1) {
+						String nombreSkin = partes[8+cont];	
+					}else {
+						String nombreSkin = partes[8+cont2];
+					}
+					cont = cantSkins+cantPersonajes;
+					cont2 = cont-cantSkins;
+					
+					
+				}
+			}
+			String region = partes[12];
 			try {
 				boolean ingresado =system.ingresarCuenta(nombreCuenta, contraseña, nick, nivelCuenta, rpCuenta, region, 0, true);
-				
+				if(ingresado) {
+					
+				}
 				if(!ingresado) {
 					System.out.println("La cuenta "+nombreCuenta+" ya existe");
 				}
