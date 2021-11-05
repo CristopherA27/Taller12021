29639af3bb698fa33ca32eb100de1de76217f46e
CristopherA27 <76230505+CristopherA27@users.noreply.github.com>
2021-11-05 00:10:40 -0300

    avance app

--- Taller1_2d02021/src/logica/App.java
+++ Taller1_2d02021/src/logica/App.java
@@ -1,5 +1,23 @@
 package logica;
 
-public class App {
+import java.io.IOException;
+import java.util.Scanner;
 
+public class App {
+	
+	
+	public static void leerPersonaje(SystemI system) throws IOException{
+		System.out.println("Leyendo personajes");
+		Scanner s = new Scanner("Personajes.txt");
+		while(s.hasNextLine()) {
+			String line = s.nextLine();
+			String [] partes = line.split(",");
+			
+		}
+		
+	}
+	
+	public static void main(String [] args) {
+		SystemI system = new SystemImpl();
+	}
 }
